946e44c127fd3c8ba30b2ad14843bce1d9208fdb
JuanModzelewski <juanmodzelewski@gmail.com>
2024-10-23 22:31:27 +0200

    Modified ProjectView query to query both Team and Project Models Updated project.html loop to display projects a user is both an author and a member of Developed a modal to retrieve projectform using htmx to create a new project

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ProjectView.as_view(), name="project"),
+    path("create_project", views.create_project, name="create_project"),
+
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
+from django.db.models import Q
 from django.views import generic
 from django.contrib import messages
+from itertools import chain
 from .models import Project, ProjectTeam
+from .forms import ProjectForm
+
 
 # Create your views here.
 class ProjectView(generic.ListView):
     template_name = "projects/projects.html"
-
-    def get_queryset(self):
-        return ProjectTeam.objects.filter(members=self.request.user)
     
+    def get_queryset(self):
+        return Project.objects.filter(Q(author=self.request.user) | Q(projectteam__members=self.request.user)).distinct()
+    
+
+
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid() and request.user.is_authenticated:
+            new_project = form.save(commit=False)
+            new_project.author = request.user
+            new_project.save()
+            messages.success(request, f"{new_project.title} added.")
+            return redirect('project')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/create_project.html', {'form': form})
+
